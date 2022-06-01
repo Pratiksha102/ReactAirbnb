e5fc90db6299d4721f7d68957c59fa3d81b105d4
Pratiksha102 <pratiksha.gupta15@gmail.com>
2022-06-01 14:08:34 +0530

    Props

---
+++ src/components/Contact.js
@@ -0,0 +1,22 @@
+import React, {useEffect} from 'react'
+
+export default function Contact(props)
+{
+
+    console.log(props)
+   
+    
+    
+    return(
+        <div className="card">
+        <img className="imgCard" src="logo192.png" />
+        <div className="container">
+          <h4><b>{props.name}</b></h4>
+          <p>{props.profession}</p>
+        </div>
+      </div>
+
+    )
+}
+
+
