5518525c79a6972b0c2eea0f66580038b987fb0f
PraneethGunas <praneethgunas404@gmail.com>
2022-06-07 10:28:09 +0530

    Latest changes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
-
 import './App.css';
 import Contact from './components/Contact';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h2>React Scrimba</h2>
-        <div className="containerReact">
-          
-        <Contact name="John Doe"
-         profession="Architect"
-        />
-         <Contact name="Luke Barnhill"
-         profession="Software Developer"
-        />
-         <Contact name="Sanjay Dighe"
-         profession="Product Manager"
-        />
-         <Contact name="Katy Simpson"
-         profession="UI Designer"
-        />
-        </div>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<h2>React Scrimba</h2>
+				<div className='containerReact'>
+					<Contact name='John Doe' profession='Architect' key={1} />
+					<Contact
+						name='Luke Barnhill'
+						profession='Software Developer'
+						key={2}
+					/>
+					<Contact name='Sanjay Dighe' profession='Product Manager' key={3} />
+					<Contact name='Katy Simpson' profession='UI Designer' key={4} />
+				</div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
